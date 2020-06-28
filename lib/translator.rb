a56f33c6a696b1bbaec4011cb01f8aaabd40eabf
Brian Lego <legobw@gmail.com>
2020-06-28 01:21:29 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -38,6 +38,7 @@ def get_japanese_emoticon(file_path, eng_emoticon)
 end
 
 def get_english_meaning(file_path, jap_emoticon)
+  
   emoticons = load_library(file_path)
   load_library(file_path) 
   english_name = ""
